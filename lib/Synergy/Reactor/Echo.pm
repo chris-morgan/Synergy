b27b179b5d6d2f904c183c1a531392833e9193ec
Ricardo Signes <rjbs@cpan.org>
2018-06-09 14:35:58 -0400

    Echo: convert to use EasyListening

--- lib/Synergy/Reactor/Echo.pm
+++ lib/Synergy/Reactor/Echo.pm
@@ -2,18 +2,16 @@ use v5.24.0;
 package Synergy::Reactor::Echo;
 
 use Moose;
-with 'Synergy::Role::Reactor';
+with 'Synergy::Role::Reactor', 'Synergy::Role::EasyListening';
 
 use experimental qw(signatures);
 use namespace::clean;
 
-sub listener_specs {
-  return {
-    name      => 'echo',
-    method    => 'echo',
-    predicate => sub ($self, $e) { $e->was_targeted },
-  };
-}
+__PACKAGE__->add_listener({
+  name      => 'echo',
+  method    => 'echo',
+  predicate => sub ($self, $e) { $e->was_targeted },
+});
 
 sub echo ($self, $event) {
   my $from_str = $event->from_user ? $event->from_user->username
